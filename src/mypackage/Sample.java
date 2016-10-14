466077b8ed31f1251d07cddd110a821fd6004da4
Rahul Kumar <rahul.kumar@bookmyshow.com>
2016-10-15 02:39:50 +0530

    vvv

--- src/mypackage/Sample.java
+++ src/mypackage/Sample.java
@@ -2,4 +2,28 @@ package mypackage;
 
 public class Sample {
 
+	public static void main(String args[]){
+	B b=(B) new A();
+		System.out.println(a.sum(10));
+	}
+	
+	
+}
+
+
+class A{
+	
+	public long sum(long a){
+		System.out.println("inside a");
+		return a;
+	}
+}
+
+
+class B extends A{
+	
+	public long sum(long a){
+		System.out.println("inside b");
+		return a;
+	}
 }
